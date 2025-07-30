60fc3ad6cb6099a5a5a834d473f28f305dc74b33
RAJ RAUTTRAY <jasminbibiworkweb45@gmail.com>
2025-07-31 03:21:37 +0530

    XQUEEN

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ASIFXQUEEN903/LUCIFER903",
+    "https://github.com/ASIFXQUEEN903/Lusicifermsuic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "LUCIFER")
 GIT_TOKEN = getenv(
